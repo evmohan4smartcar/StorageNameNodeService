5b521b7155ced2b8a7af70eccb12185ed13f78fb
evmohanSmartcar <ev.mohan@intellicar.in>
2021-03-06 16:27:43 +0530

    Initial commit

---
+++ src/main/java/in/intellicar/layer5/service/namenode/StorageNameNodeService.java
@@ -0,0 +1,15 @@
+package in.intellicar.layer5.service.namenode;
+
+/**
+ * @author krishna mohan
+ * @version 1.0
+ * @project StorageNameNodeService
+ * @date 06/03/21 - 4:22 PM
+ */
+public class StorageNameNodeService
+{
+    public static void main(String args[])
+    {
+
+    }
+}
