dafd093d8cb84605d5091f90c1e471bd5098849e
Naveen Dandiprolu <naveend@pinclick.com>
2021-03-08 13:36:32 +0530

    Added NameNodeUtils.java

--- src/main/java/in/intellicar/layer5/service/namenode/mysql/NameNodePayloadHandler.java
+++ src/main/java/in/intellicar/layer5/service/namenode/mysql/NameNodePayloadHandler.java
@@ -3,7 +3,12 @@ package in.intellicar.layer5.service.namenode.mysql;
 import in.intellicar.layer5.beacon.storagemetacls.PayloadTypes;
 import in.intellicar.layer5.beacon.storagemetacls.StorageClsMetaPayload;
 import in.intellicar.layer5.beacon.storagemetacls.payload.StorageClsMetaErrorRsp;
+import in.intellicar.layer5.beacon.storagemetacls.payload.namenodeservice.client.AccIdGenerateReq;
+import in.intellicar.layer5.beacon.storagemetacls.payload.namenodeservice.client.AccIdGenerateRsp;
+import in.intellicar.layer5.beacon.storagemetacls.payload.namenodeservice.internal.AccIdRegisterReq;
+import in.intellicar.layer5.beacon.storagemetacls.payload.namenodeservice.internal.AccIdRegisterRsp;
 import in.intellicar.layer5.beacon.storagemetacls.service.common.IPayloadRequestHandler;
+import in.intellicar.layer5.service.namenode.utils.NameNodeUtils;
 import in.intellicar.layer5.utils.sha.SHA256Item;
 import io.vertx.core.Future;
 import io.vertx.mysqlclient.MySQLPool;
@@ -24,21 +29,20 @@ public class NameNodePayloadHandler implements IPayloadRequestHandler {
 
         switch (payloadType) {
             case ACCOUNT_ID_GEN_REQ:
-//                Future<SHA256Item> accountIDFuture = NameNodeUtils.getAccountID((AccountIDReq) lRequestPayload, lVertxMySQLClient, lLogger);
-//                if (accountIDFuture.succeeded()) {
-//                    return new AccountIDRsp((AccountIDReq) lRequestPayload, accountIDFuture.result());
-//                } else {
-//                    return new StorageClsMetaErrorRsp(accountIDFuture.cause().getLocalizedMessage(), lRequestPayload);
-//                }
-                return null;
+                Future<SHA256Item> accountIDFuture = NameNodeUtils.generateAccountID((AccIdGenerateReq) lRequestPayload, lVertxMySQLClient, lLogger);
+                if (accountIDFuture.succeeded()) {
+                    return null;
+//                    return accountIDFuture.result();
+                } else {
+                    return new StorageClsMetaErrorRsp(accountIDFuture.cause().getLocalizedMessage(), lRequestPayload);
+                }
             case ACCOUNT_ID_REG_REQ:
-//                Future<SHA256Item> nsIDFuture = NameNodeUtils.getNsId((NsIdReq) lRequestPayload, lVertxMySQLClient, lLogger);
-//                if (nsIDFuture.succeeded()) {
-//                    return new NsIdRsp((NsIdReq) lRequestPayload, nsIDFuture.result());
-//                } else {
-//                    return new StorageClsMetaErrorRsp(nsIDFuture.cause().getLocalizedMessage(), lRequestPayload);
-//                }
-                return null;
+                Future<AccIdRegisterRsp> ack = NameNodeUtils.registerAccountID((AccIdRegisterReq) lRequestPayload, lVertxMySQLClient, lLogger);
+                if (ack.succeeded()) {
+                    return ack.result();
+                } else {
+                    return new StorageClsMetaErrorRsp(ack.cause().getLocalizedMessage(), lRequestPayload);
+                }
             default:
                 return new StorageClsMetaErrorRsp("Sent Unknown PayloadType",  lRequestPayload);
         }
---
+++ src/main/java/in/intellicar/layer5/service/namenode/utils/NameNodeUtils.java
@@ -0,0 +1,118 @@
+package in.intellicar.layer5.service.namenode.utils;
+
+import in.intellicar.layer5.beacon.storagemetacls.payload.namenodeservice.client.AccIdGenerateReq;
+import in.intellicar.layer5.beacon.storagemetacls.payload.namenodeservice.client.AccIdGenerateRsp;
+import in.intellicar.layer5.beacon.storagemetacls.payload.namenodeservice.internal.AccIdRegisterReq;
+import in.intellicar.layer5.beacon.storagemetacls.payload.namenodeservice.internal.AccIdRegisterRsp;
+import in.intellicar.layer5.utils.LittleEndianUtils;
+import in.intellicar.layer5.utils.sha.SHA256Item;
+import in.intellicar.layer5.utils.sha.SHA256Utils;
+import io.vertx.core.Future;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.Tuple;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Logger;
+
+public class NameNodeUtils {
+
+    //TODO
+
+    public static Future<SHA256Item> generateAccountID(AccIdGenerateReq req, MySQLPool vertxMySQLClient, Logger logger){
+        String accountName = LittleEndianUtils.printHexArray(req.accNameUtf8Bytes);
+
+        Future<SHA256Item> checkedAccountID = checkAccountID(accountName, vertxMySQLClient, logger);
+        if (checkedAccountID.succeeded()) {
+            return checkedAccountID;
+        } else {
+            String salt = Long.toHexString(System.nanoTime());
+            SHA256Item accountIDSHA = generateAccountID(req.accNameUtf8Bytes, salt.getBytes());
+
+            //TODO: Update db.table_name
+            Future<RowSet<Row>> insertFuture = vertxMySQLClient.preparedQuery("INSERT INTO accounts.account_info (account_name, salt, account_id, ack) values (?, ?, ?, ?)")
+                    .execute(Tuple.of(accountName, salt, accountIDSHA.toHex(), 0));
+
+            while (true) {
+                synchronized (insertFuture) {
+
+                    try {
+                        insertFuture.wait(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (insertFuture.succeeded()) {
+                    return Future.succeededFuture(accountIDSHA);
+                } else {
+                    return Future.failedFuture(insertFuture.cause());
+                }
+            }
+        }
+    }
+
+    //TODO
+    public static Future<AccIdRegisterRsp> registerAccountID(AccIdRegisterReq req,  MySQLPool vertxMySQLClient, Logger logger){
+        return null;
+    }
+
+
+    // Utility Functions
+    public static Future<SHA256Item> checkAccountID(String accountName, MySQLPool vertxMySQLClient, Logger logger) {
+
+        //TODO update db.table_name
+        String sql = "SELECT account_id from accounts.account_info where account_name = '" + accountName + "'";
+        Future<RowSet<Row>> future = vertxMySQLClient
+                .query(sql)
+                .execute();
+        while (true) {
+            synchronized (future) {
+
+                try {
+                    future.wait(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (future.isComplete()) {
+                RowSet<Row> rows = future.result();
+                if (rows != null && rows.size() > 0) {
+                    String hexID = rows.iterator().next().getString("account_id"); // TODO update column name
+                    SHA256Item accountIDSHA = new SHA256Item(LittleEndianUtils.hexStringToByteArray(hexID));
+                    return Future.succeededFuture(accountIDSHA);
+                } else {
+                    return Future.failedFuture(future.cause());
+                }
+            }
+        }
+    }
+
+    public static SHA256Item generateAccountID(byte[] nameBytes, byte[] saltBytes) {
+
+        byte[] saltyName = new byte[saltBytes.length + nameBytes.length];
+        System.arraycopy(nameBytes, 0, saltyName, 0, nameBytes.length);
+        System.arraycopy(saltBytes, 0, saltyName, nameBytes.length, saltBytes.length);
+
+        try {
+            return SHA256Utils.getSHA256(saltyName);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+/*
+TODO: Client End
+1. Get InstanceId corresponding to accountName
+2. Send AccIdRegisterReq to InstanceId
+**/
+    public static Future<SHA256Item> getInstanceID(SHA256Item accountID){
+        return null;
+    }
+
+    public static Future<Integer> getAck() {
+        return Future.succeededFuture(1);
+    }
+
+}
